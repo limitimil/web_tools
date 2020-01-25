693a20845ca3bf5c6178aacd945e30c93561b4b7
limitimil <zlsh10340@gmail.com>
2020-01-25 20:58:31 +0800

    VUE: ROUTER: APP.vue: expose new feature page

---
+++ vue/src/components/SumDigits.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="sum-digits"> HelloWorld</div>
+</template>
+
+<script lang="ts">
+import Vue, { PropType } from 'vue';
+
+export default Vue.extend({
+
+})
+</script>
+
+<style scoped lang="less">
+</style>
--- vue/src/router/index.ts
+++ vue/src/router/index.ts
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
+// components
+import SumDigits from '../components/SumDigits.vue';
 
 Vue.use(VueRouter);
 
@@ -17,6 +19,11 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+  },
+  {
+    path: '/utils/sum_digits',
+    name: 'sum_digits',
+    component: SumDigits,
   },
 ];
 
