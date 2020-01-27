d27fd5e348788ef71cb744de08faa7c084bd0fa8
limitimil <zlsh10340@gmail.com>
2020-01-27 16:18:31 +0800

    PACKAGE: iview

--- vue/src/main.ts
+++ vue/src/main.ts
@@ -2,8 +2,11 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+// global use packages
+import iview from 'iview'
 
 Vue.config.productionTip = false;
+Vue.use(iview);
 
 new Vue({
   router,
