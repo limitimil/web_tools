5c2aeff8eb95cb180725f70257b05ddfbce0960c
limitimil <zlsh10340@gmail.com>
2020-01-27 16:59:20 +0800

    PACKAGE: install axios

--- vue/src/main.ts
+++ vue/src/main.ts
@@ -3,10 +3,14 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 // global use packages
-import iview from 'iview'
+import iview from 'iview';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
 
 Vue.config.productionTip = false;
 Vue.use(iview);
+axios.defaults.baseURL = 'http://localhost:5000/';
+Vue.use(VueAxios, axios);
 
 new Vue({
   router,
